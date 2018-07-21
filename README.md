718d76224d8d540397287258b1ebc43b2a05e7de
Duncan McGreggor <oubiwann@gmail.com>
2018-07-20 22:26:49 -0500

    Copied parts of project to async-udp.

---
+++ README.md
@@ -0,0 +1,135 @@
+# Tímı
+
+A time tracker written in Clojure(Script).
+
+![Tími screenshot](docs/screenshot-2.png)
+
+This project is 100% based on the source code of the
+[Infi Alibi Time Tracker](https://github.com/infi-nl/alibi). We've made a
+handful of changes for our own needs and aesthetics, but the project owes all
+of its DNA to the original.
+
+
+## Features
+
+This is far from a fully fledged time tracker, yet it has the following
+features, and more is on the way!
+
+* Categorize time entries by project and task.
+* Graphical overview of your weekly activity.
+* Indicate whether entries are billable or not.
+* Tasks have an associated billing method, either fixed price, overhead or hourly.
+* Create projects and tasks via a CLI, and the REPL.
+* Extension points for a different data source implementations.
+* OpenID Connect and single user authentication.
+* Out of the box Sqlite3 database support.
+
+
+## Dependencies
+
+The prerequisites for this route are:
+
+- Have a Git client installed.
+- Make sure Java is installed.
+- Make sure [lein](https://leiningen.org/) is installed.
+- Have `npm` installed
+- Ensure that `make` is installed
+
+
+## Preparation
+
+### Initialization
+
+Clone the repo. Then, inside the repo, initialize a time tracker:
+
+```
+$ make var/data/timi.db
+```
+
+This will create a sqlite3 db with filename `var/data/timi.db`, which contains
+the right schema, but has no data yet.
+
+
+### Configuration
+
+Before we create some data, you first need to setup a config file, and for
+that you need to create a key for cookie encryption, e.g. using:
+
+```
+$ make cookie
+```
+
+Then execute the following to create a config file, replacing the `:cookie-
+encryption-key` value with your own key and `[:sqlite :subname]` with the
+database file you picked in a previous step.
+
+```
+$ cat << EOF > config/local/config.edn
+{:cookie-encryption-key "xxxxxxxxxxxxxxxx"
+ :selmer-caching? false
+
+ :persistence :sqlite
+ :persistence-strategies {}
+ :sqlite {:subprotocol "sqlite" :subname "var/data/timi.db"}
+
+ :authentication :single-user
+ :single-user {:username "Alice"}}
+EOF
+```
+
+
+## Build
+
+The Clojure and ClojureScript for the project can be built in one go with the
+following:
+
+```
+$ make
+```
+
+If you just want to do one or the other, you may use `make build-clj` or
+`make build-cljs`.
+
+
+### Projects Population
+
+Now we can populate the database with some projects and tasks. Here's how you
+create a project:
+
+```
+$ lein timi-create-project "Tími" :billing-method :hourly
+```
+```clj
+Creating new project:
+{:project-name "Tími", :billing-method :hourly}
+Done. Id for project is 1
+```
+
+The project ID that is returned is what you will use when creating tasks for
+that project. For example:
+
+```
+$ lein timi-create-task "Project management" :for-project 1 :billing-method :overhead
+$ lein timi-create-task "Programming" :for-project 1
+```
+
+
+## Startup
+
+Run the server:
+
+```
+$ make run
+```
+
+If everything went well, there should now be a web server running on
+[http://localhost:3000](http://localhost:3000). Navigate to it and have fun!
+
+
+## License
+
+Copyright © 2017 Infi Holding B.V. and contributers.
+
+Copyright © 2017 Billo Systems, Ltd. Co.
+
+Distributed under the Mozilla Public License Version 2.0
---
+++ project.clj
@@ -0,0 +1,135 @@
+(defproject timi "0.2.0-SNAPSHOT"
+  :description "A Clojure(Script)-based time tracker"
+  :url "https://github.com/billosys/timi"
+  :license {
+    :name "Mozilla Public License Version 2.0"
+    :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :min-lein-version "2.7.1"
+  :dependencies [
+    [bouncer "1.0.1" :exclusions [com.andrewmcveigh/cljs-time]]
+    [buddy/buddy-auth "1.4.1"]
+    [buddy/buddy-sign "1.5.0"]
+    [clj-http "2.1.0" :exclusions [commons-io]]
+    [clj-time "0.13.0" :exclusions [joda-time]]
+    [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
+    [clojusc/trifl "0.1.0-SNAPSHOT"]
+    [clojusc/twig "0.3.2-SNAPSHOT"]
+    [com.andrewmcveigh/cljs-time "0.4.0"]
+    [com.stuartsierra/component "0.3.2"]
+    [compojure "1.6.0"]
+    [crypto-random "1.2.0"]
+    [http-kit "2.2.0"]
+    [mysql/mysql-connector-java "6.0.6"]
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/data.json "0.2.6"]
+    [org.clojure/java.jdbc "0.7.0-alpha2"]
+    [org.webjars/bootstrap-datepicker "1.6.4"]
+    [org.webjars/bootswatch-superhero "3.3.7"]
+    [org.webjars/font-awesome "4.7.0"]
+    [org.webjars/jquery "3.2.0"]
+    [org.xerial/sqlite-jdbc "3.18.0"]
+    [ring-logger "0.7.7"]
+    [ring-webjars "0.2.0"]
+    [ring.middleware.conditional "0.2.0"]
+    [ring/ring-codec "1.0.1"]
+    [ring/ring-core "1.6.1"]
+    [selmer "1.10.7" :exclusions [joda-time]]]
+  :repl-options {
+    :init-ns timi.dev}
+  :plugins [
+    [cider/cider-nrepl "0.10.0"]
+    [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]
+    [lein-figwheel "0.5.8"]
+    [lein-pprint "1.1.1"]
+    [lein-ring "0.9.7"]]
+  :source-paths ["src/clj"]
+  :main timi.server.core
+  :cljsbuild {
+    :builds
+      [{:id "dev"
+        :source-paths ["src/cljs"]
+        :figwheel {}
+        :compiler {
+          :main timi.client.core
+          :asset-path "/dist/cljs/out"
+          :output-to "resources/public/dist/cljs/timi.js"
+          :output-dir "resources/public/dist/cljs/out"
+          :source-map-timestamp true
+          :language-in :ecmascript5
+          :preloads [devtools.preload]}}
+       {:id "single-file"
+        :source-paths ["src/cljs"]
+        :compiler {
+          :output-to "resources/public/dist/cljs/timi.js"
+          :main timi.client.core
+          :output-dir "resource/public/dist/cljs/out-single-file"
+          :optimizations :simple
+          :language-in :ecmascript5
+          :pretty-print true}}
+       {:id "min"
+        :source-paths ["src/cljs"]
+        :compiler {
+          :output-to "resources/public/dist/cljs/timi.js"
+          :main timi.client.core
+          :optimizations :advanced
+          :externs [
+            "externs/js-joda.js"
+            "externs/datepicker.js"
+            "externs/selectize.js"]
+          ;:pseudo-names true
+          :verbose true
+          :language-in :ecmascript5
+          :pretty-print false}}]}
+  :figwheel {
+    :server-port 5076
+    :css-dirs ["resources/public/css"]
+    :server-logfile "var/logs/figwheel.log"}
+  :profiles {
+    :uberjar {
+      :aot [#"timi.server.*" #"timi.config.*"]}
+    :dev [{
+      :dependencies [
+        [binaryage/devtools "0.9.4"]
+        [com.cemerick/piggieback "0.2.1"]
+        [figwheel-sidecar "0.5.10"]
+        [org.clojure/clojurescript "1.9.542"]
+        [org.clojure/core.async "0.3.442"
+         :exclusions [org.clojure/tools.reader]]
+        [org.omcljs/om "0.9.0"]
+        [ring/ring-mock "0.3.0"]]
+      :source-paths [
+        "dev-resources/src"]
+      :repl-options {
+        :init (set! *print-length* 50)
+        :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+      :resource-paths ["config/dev"]}
+     :custom-persistence]
+    :test [{
+      :resource-paths ["config/test"]
+      :plugins [
+        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
+        [lein-ancient "0.6.10"]]}
+     :custom-persistence]
+    :local [{:resource-paths ["config/local"]}
+            :custom-persistence]
+    :demo [{:resource-paths ["config/demo"]}
+           :custom-persistence]
+    :prod [{:resource-paths ["config/prod"]}
+           :custom-persistence]
+    :custom-persistence {}}
+  :aliases {
+    "timi-help" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli"]
+    "timi-init" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "sqlite" "create-db" :filename]
+    "timi-create-project" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "projects" "create" :name]
+    "timi-create-task" [
+      "with-profile" "+local"
+      "run" "-m" "timi.cli" "tasks" "create" :name]
+    "timi-run" ["with-profile" "+local" "run"]
+    "check-deps" ["with-profile" "+test" "ancient" "check" "all"]})
