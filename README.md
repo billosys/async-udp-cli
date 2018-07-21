4169986afea80cec61e5829ea90649b225d7905d
Duncan McGreggor <oubiwann@gmail.com>
2018-07-20 22:16:21 -0500

    Updated project files.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Tímı
+# async-udp
 [![Build Status][travis-badge]][travis][![Clojars Project][clojars-badge]][clojars][![Clojure version][clojure-v]](project.clj)
 
-*A time tracker written in Clojure(Script)*
+*A Clojure utility library for async UDP clients/servers*
 
 [![][logo]][logo-large]
 
@@ -9,246 +9,38 @@
 #### Contents
 
 * [About](#about-)
-* [Screenshot](#screenshot-)
-* [Features](#features-)
-* [Dependencies](#dependencies-)
-* [Documentation](#documentation-)
 * [Usage](#usage-)
-  * [Build](#build-)
-  * [Configuration](#configuration-)
-  * [Starting](#starting-)
-  * [CLI Server](#cli-server-)
-  * [Data Initialization](#data-initialization-)
-  * [Projects and Tasks Population](#projects-and-tasks-population-)
-* [History](#history-)
 * [License](#license-)
 
 
 ## About [&#x219F;](#contents)
 
-Tímı is a project time-tracking tool intended for use by individuals,
-non-profits, and small companies, originally based on
-[Infi Alibi Time Tracker](https://github.com/infi-nl/alibi) (more on
-that in the [History](#history-) section below.)
-
-
-## Screenshot [&#x219F;](#contents)
-
-Tímı in action:
-
-![Tími screenshot](resources/docs/images/screenshot-2.png)
-
-
-## Features [&#x219F;](#contents)
-
-This is far from a fully fledged time tracker, yet it has the following
-features, and more is on the way!
-
-* Categorize time entries by project and task.
-* Graphical overview of your weekly activity.
-* Indicate whether entries are billable or not.
-* Tasks have an associated billing method, either fixed price, overhead or hourly.
-* Create projects and tasks via a CLI, and the REPL.
-* Extension points for a different data source implementations.
-* OpenID Connect and single user authentication.
-* Out of the box Sqlite3 database support.
-
-
-## Dependencies [&#x219F;](#contents)
-
-The prerequisites for this route are:
-
-- Have a Git client installed.
-- Make sure Java is installed.
-- Make sure [lein](https://leiningen.org/) is installed.
-- Have `npm` installed
-- Ensure that `make` is installed
-
-
-## Documentation [&#x219F;](#contents)
-
-In addition to this README, there is documentation availble once the
-application is running:
-
-* http://localhost:5099/docs
+TBD
 
 
 ## Usage [&#x219F;](#contents)
 
-
-### Build [&#x219F;](#contents)
-
-Before you can do much with Tímı, you'll need to build it. As long as you
-have all the dependencies install on your system (see above), it's as simple
-as this:
-
-```
-$ make
-```
-
-This will do a lot, but can be summarized with the following:
-
-* Download all the Clojure and JavaScript (Node.js) dependencies
-* Build the server code
-* Build the client code
-* Build the CLI tool
-
-
-### Configuration [&#x219F;](#contents)
-
-Before we create some data, you first need to setup a config file. For local
-development, copy the sample config:
-
-```
-$ cp config/sample/config.edn config/local
-```
-
-You'll want to change the encryption cookie, though. Generate a new one with:
-
-```
-$ make cookie
-```
-
-Then update your new `config/local/config.edn` file, replacing the same value
-for `:cookie-encryption-key` with the one `make cookie` generated for you.
-
-
-### Starting [&#x219F;](#contents)
-
-With the application built, you're ready to start it. If you just want to
-use the application and not do any development work, simply do:
-
-```
-$ make run
-```
-
-If you'll be doing server-side development and want access to the REPL:
-
-```
-$ make repl
-```
-
-Once the REPL finishes loading, start the application with `(run)`. You can
-reload new code and restart the application by typing `(reset)`. The server
-running in the REPL will be available on
-[http://localhost:5098](http://localhost:5098)
-
-If you'll be doing client-side development, then do:
-
-```
-$ make dev
-```
-
-If you then load up the application in your browser at
-[http://localhost:5099](http://localhost:5099),
-you'll see a prompt appear back in your terminal: this is ClojureScript, and
-you have access to the client-side of the application, as it's running in your
-browser.
-
-
-### CLI Server  [&#x219F;](#contents)
-
-One of the services that is started when the app comes up is a command-line
-server, listening on port 5097. The Tímı cli tool that gets installed to
-`./bin/timi` connects to this server to issue your commands and return results.
-This feature allows for command-line operations that are much, much faster
-than usual JVM commands (since there's no start-up cost for every command).
-
-Now make sure you have easy access to `timi` by adding its location to
-your `PATH`:
-
-```
-$ export PATH=$PATH:`pwd`/bin
-```
-
-There's also an auto-completion script you can use to get commands and
-subcommands at the double-tap of the `<TAB>` key:
-
-```
-$ source resources/bash/timi-auto-complete
-```
-
-
-### Data Initialization [&#x219F;](#contents)
-
-Database setup is done using the `timi` CLI tool:
-
-```
-$ timi db init
-```
-
-If you are using the values defined for you in the sample config, this will
-create a sqlite3 db with filename `var/data/timi.db`, which contains
-the right schema, but has no data yet.
-
-If you would like to override your configuration value. you can do the
-following:
-
-```
-$ timi db init my/path/timi.db
-```
-
-Note, however, that you will need to update your configuration file to point
-to the new location of the database.
-
-
-### Projects and Tasks Population [&#x219F;](#contents)
-
-Now we can populate the database with some projects and tasks. Here's how you
-create a project:
-
-```
-$ lein timi-create-project "Tími" :billing-method :hourly
-```
-```clj
-Creating new project:
-{:project-name "Tími", :billing-method :hourly}
-Done. Id for project is 1
-```
-
-The project ID that is returned is what you will use when creating tasks for
-that project. For example:
-
-```
-$ lein timi-create-task "Project management" :for-project 1 :billing-method :overhead
-$ lein timi-create-task "Programming" :for-project 1
-```
-
-At this point, you're ready to start tracking time! Visit either
-[http://localhost:5098](http://localhost:5098) or
-[http://localhost:5099](http://localhost:5099), depending upon how you have
-started Tímı.
-
-
-## History [&#x219F;](#contents)
-
-This project started as a fork of the
-[Infi Alibi Time Tracker](https://github.com/infi-nl/alibi), but has since
-undergone major refactorings (with more to come). What started as a
-handful of changes for our own needs and aesthetics has evolved into something
-that is a new application in its own right. Still, Tímı owes much of its DNA
-and all of its beginnings to Unfi's Alibi.
+TBD
 
 
 ## License [&#x219F;](#contents)
 
-Copyright © 2017 Infi Holding B.V. and contributers.
 
-Copyright © 2017 Billo Systems, Ltd. Co.
+Copyright © 2017-2018 Billo Systems, Ltd. Co.
 
-Distributed under the Mozilla Public License Version 2.0
+Distributed under the Apache License Version 2.0.
 
 
 <!-- Named page links below: /-->
 
-[travis]: https://travis-ci.org/billosys/timi
-[travis-badge]: https://travis-ci.org/billosys/timi.png?branch=master
-[deps]: http://jarkeeper.com/billosys/timi
-[deps-badge]: http://jarkeeper.com/billosys/timi/status.svg
+[travis]: https://travis-ci.org/billosys/async-udp
+[travis-badge]: https://travis-ci.org/billosys/async-udp.png?branch=master
+[deps]: http://jarkeeper.com/billosys/async-udp
+[deps-badge]: http://jarkeeper.com/billosys/async-udp/status.svg
 [logo]: resources/public/images/golden-clock-square-250px.png
 [logo-large]: resources/public/images/golden-clock-square-2400px.png
-[tag-badge]: https://img.shields.io/github/tag/billosys/timi.svg
-[tag]: https://github.com/billosys/timi/tags
-[clojure-v]: https://img.shields.io/badge/clojure-1.8.0-blue.svg
-[clojars]: https://clojars.org/systems.billo/timi
-[clojars-badge]: https://img.shields.io/clojars/v/systems.billo/timi.svg
+[tag-badge]: https://img.shields.io/github/tag/billosys/async-udp.svg
+[tag]: https://github.com/billosys/async-udp/tags
+[clojure-v]: https://img.shields.io/badge/clojure-1.9.0-blue.svg
+[clojars]: https://clojars.org/systems.billo/async-udp
+[clojars-badge]: https://img.shields.io/clojars/v/systems.billo/async-udp.svg
--- project.clj
+++ project.clj
@@ -1,194 +1,53 @@
-(defproject systems.billo/timi "0.4.0-SNAPSHOT"
-  :description "A Clojure(Script)-based time tracker"
-  :url "https://github.com/billosys/timi"
+(defproject systems.billo/async-udp "0.1.0-SNAPSHOT"
+  :description "A Clojure utility library for async UDP clients/servers"
+  :url "https://github.com/billosys/async-udp"
   :license {
-    :name "Mozilla Public License Version 2.0"
-    :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :signing {:gpg-key "li@billo.systems"}
-  :min-lein-version "2.7.1"
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :min-lein-version "2.8.1"
   :dependencies [
-    [bouncer "1.0.1" :exclusions [com.andrewmcveigh/cljs-time]]
-    [buddy/buddy-auth "1.4.1"]
-    [buddy/buddy-sign "1.5.0"]
-    [clj-http "2.1.0" :exclusions [commons-io]]
-    [clj-time "0.13.0" :exclusions [joda-time]]
-    [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
-    [clojusc/trifl "0.1.0-SNAPSHOT"]
-    [clojusc/twig "0.3.1"]
-    [com.andrewmcveigh/cljs-time "0.4.0"]
-    [com.stuartsierra/component "0.3.2"]
-    [compojure "1.6.0"]
-    [crypto-random "1.2.0"]
-    [http-kit "2.2.0"]
-    [markdown-clj "0.9.99"]
-    [mysql/mysql-connector-java "6.0.6"]
-    [org.clojure/clojure "1.8.0"]
-    [org.clojure/data.json "0.2.6"]
-    [org.clojure/java.jdbc "0.7.0-alpha2"]
-    [org.clojure/tools.cli "0.3.5"]
-    [org.webjars/bootstrap-datepicker "1.6.4"]
-    [org.webjars/bootswatch-superhero "3.3.7"]
-    [org.webjars/font-awesome "4.7.0"]
-    [org.webjars/jquery "3.2.0"]
-    [org.xerial/sqlite-jdbc "3.18.0"]
-    [ring-logger "0.7.7"]
-    [ring-webjars "0.2.0"]
-    [ring.middleware.conditional "0.2.0"]
-    [ring/ring-codec "1.0.1"]
-    [ring/ring-core "1.6.1"]
-    [selmer "1.10.7" :exclusions [joda-time]]
-    [systems.billo/inet-address "0.1.0"]
-    [systems.billo/sockets "0.1.0"]]
-  :plugins [
-    [cider/cider-nrepl "0.10.0"]
-    [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]
-    [lein-figwheel "0.5.10"]
-    [lein-pprint "1.1.1"]
-    [lein-ring "0.9.7"]]
-  :source-paths ["src/clj"]
-  :main timi.server.core
-  :cljsbuild {
-    :builds
-      [{:id "dev"
-        :source-paths ["src/cljs/app"]
-        :figwheel {}
-        :compiler {
-          :main timi.client.core
-          :asset-path "/dist/cljs/out"
-          :output-to "resources/public/dist/cljs/timi.js"
-          :output-dir "resources/public/dist/cljs/out"
-          :source-map-timestamp true
-          :language-in :ecmascript5
-          :preloads [devtools.preload]}}
-       {:id "single-file"
-        :source-paths ["src/cljs/app"]
-        :compiler {
-          :output-to "resources/public/dist/cljs/timi.js"
-          :main timi.client.core
-          :output-dir "resource/public/dist/cljs/out-single-file"
-          :optimizations :simple
-          :language-in :ecmascript5
-          :pretty-print true}}
-       {:id "min"
-        :source-paths ["src/cljs/app"]
-        :compiler {
-          :output-to "resources/public/dist/cljs/timi.js"
-          :main timi.client.core
-          :optimizations :advanced
-          :externs [
-            "externs/js-joda.js"
-            "externs/datepicker.js"
-            "externs/selectize.js"]
-          ;:pseudo-names true
-          :verbose true
-          :language-in :ecmascript5
-          :pretty-print false}}
-        {:id "cli"
-         :source-paths ["src/cljs/cli"]
-         :compiler {
-           :output-to "bin/timi"
-           :output-dir "target/cljs/cli"
-           :optimizations :simple
-           :pretty-print true
-           :main timi.client.cli
-           :target :nodejs
-           :npm-deps {:colors "1.1.2"}
-           :verbose true}}]}
-  :figwheel {
-    :server-port 5099
-    :ring-handler timi.server.core/app
-    :css-dirs ["resources/public/css"]
-    ;:server-logfile "var/logs/figwheel.log"
-    :server-logfile false}
+    [org.clojure/clojure "1.9.0"]]
   :profiles {
-    :uberjar {
-      :aot [#"timi.server.*" #"timi.config.*"]}
-    :docs {
-      :aot :all
-      :dependencies [[systems.billo/superhero-codox-theme "0.3.0-SNAPSHOT"]]
+    :ubercompile {
+      :aot :all}
+    :lint {
       :plugins [
-        [lein-codox "0.10.3"]]}
-    :server-docs {
-      :codox {
-        :project {:name "Tímı"}
-        :themes [:superhero]
-        :output-path "resources/public/static/docs/api/server/current"
-        :doc-paths ["resources/docs/api"]
-        :metadata {:doc/format :markdown}}}
-    :client-docs {
-      :codox {
-        :project {:name "Tímı"}
-        :themes [:superhero]
-        :language :clojurescript
-        :source-paths ["src/cljs"]
-        :output-path "resources/public/static/docs/api/client/current"
-        :doc-paths ["resources/docs/api"]
-        :metadata {:doc/format :markdown}}}
-    :dev [{
+        [jonase/eastwood "0.2.8"]
+        [lein-ancient "0.6.15"]]}
+    :clojure {
+      :source-paths ["src/clj"]
       :dependencies [
-        [binaryage/devtools "0.9.4"]
-        [com.cemerick/piggieback "0.2.1"]
-        [figwheel-sidecar "0.5.10"]
-        [org.clojure/clojurescript "1.9.542"]
-        [org.clojure/core.async "0.3.442"
-         :exclusions [org.clojure/tools.reader]]
-        [org.omcljs/om "0.9.0"]
-        [ring/ring-mock "0.3.0"]]
-      :source-paths [
-        "dev-resources/src"]
-      :repl-options {
-        :init (set! *print-length* 50)
-        :init-ns timi.dev
-        :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-      :resource-paths ["config/dev"]
-      :clean-targets ^{:protect false}
-        ["resources/public/dist/cljs" :target-path]}
-     :custom-persistence]
-    :test [{
-      :resource-paths ["config/test"]
+        [clojusc/trifl "0.3.0"]
+        [clojusc/twig "0.3.3"]
+        [org.clojure/core.async "0.4.474"]
+        [systems.billo/inet-address "0.1.1"]
+        [systems.billo/sockets "0.1.1"]]}
+    :clojurescript {
+      :dependencies [
+        [org.clojure/clojurescript "1.10.339"]]
       :plugins [
-        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-        [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-        [lein-ancient "0.6.10"]]}
-     :custom-persistence]
-    :local [{:resource-paths ["config/local"]}
-            :custom-persistence]
-    :demo [{:resource-paths ["config/demo"]}
-           :custom-persistence]
-    :build [{:resource-paths ["config/sample"]}
-           :custom-persistence]
-    :prod [{:resource-paths ["config/prod"]}
-           :custom-persistence]
-    :custom-persistence {}}
-  ;; XXX Note: most (if not all) of the aliases are going to change or go away
-  ;;     once we switch to the new CLI
+        [lein-cljsbuild "1.1.7"]]
+      :cljsbuild {
+        :builds
+          [{:id "cli"
+            :source-paths ["src/cljs/billo"]
+            :compiler {
+              :output-to "bin/example"
+              :output-dir "target/cljs/billo"
+              :optimizations :simple
+              :pretty-print true
+              :main billo.udp.example
+              :target :nodejs
+              :verbose true}}]}}}
   :aliases {
-    "timi-create-project" [
-      "with-profile" "+local"
-      "run" "-m" "timi.cli" "projects" "create" :name]
-    "timi-create-task" [
-      "with-profile" "+local"
-      "run" "-m" "timi.cli" "tasks" "create" :name]
-    "timi-run" ["with-profile" "+local" "run"]
-    "timi-figwheel" ["with-profile" "+local" "figwheel"]
-    "check-deps" ["with-profile" "+test" "ancient" "check" "all"]
-    "lint" ["with-profile" "+test" "kibit"]
-    "timi-build" ["with-profile" "+build" "do"
-      ;["check-deps"] XXX enable once clj-http 3.5.x is working with Tímı
-      ["lint"]
-      ["test"]
-      ["compile"]
-      ["uberjar"]
-      ["cljsbuild" "once"]
-      ["cljsbuild" "once" "cli"]]
-    "timi-deploy" ["with-profile" "build" "deploy" "clojars"]
-    "timi-server-docs" ["with-profile" "+build,+docs,+server-docs" "codox"]
-    "timi-client-docs" ["with-profile" "+build,+docs,+client-docs" "codox"]
-    "timi-docs" ["do" ["timi-server-docs"] ["timi-client-docs"]]}
-  :repl-options {
-    :prompt (fn [ns] (str "\u001B[35m[\u001B[34m"
-                          ns
-                          "\u001B[35m]\u001B[33m λ:\u001B[m "))
-    :welcome ~(do
-                (println (slurp "resources/text/banner.txt"))
-                (println (slurp "resources/text/loading.txt")))})
+    "ubercompile" ["with-profile" "+ubercompile,+clojure" "uberjar"]
+    "uberjar" ["with-profile" "+clojure" "uberjar"]
+    "deploy" ["with-profile" "+clojure" "deploy"]
+    "build-bin" ["with-profile" "+clojurescript" "cljsbuild" "once" "cli"]
+    "check-vers" ["with-profile" "+lint" "ancient" "check" ":all"]
+    "check-jars" ["with-profile" "+lint" "do"
+      ["deps" ":tree"]
+      ["deps" ":plugin-tree"]]
+    "check-deps" ["do"
+      ["check-jars"]
+      ["check-vers"]]})
